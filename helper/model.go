86c96cc51e1902999728d8e5b68c81645f4ceca2
Artamananda <artamananda@gmail.com>
2023-08-05 21:36:07 +0700

    update helper

--- helper/model.go
+++ helper/model.go
@@ -12,3 +12,11 @@ func ToMessageResponse(message domain.Message) web.MessageResponse {
 		CreatedAt: message.CreatedAt,
 	}
 }
+
+func ToMessageResponses(messages []domain.Message) []web.MessageResponse {
+	var messageResponses []web.MessageResponse
+	for _, message := range messages {
+		messageResponses = append(messageResponses, ToMessageResponse(message))
+	}
+	return messageResponses
+}
